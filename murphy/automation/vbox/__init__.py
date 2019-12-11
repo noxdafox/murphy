8bcce5c24fd9c8180ee3eb82a004184dc87d3d0d
Matteo Cafasso <noxdafox@gmail.com>
2019-12-11 22:18:57 +0200

    automation/vbox: remove legacy Virtualbox support
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- murphy/automation/vbox/__init__.py
+++
@@ -1,6 +0,0 @@
-from murphy.automation.vbox.load import VirtualboxLoad
-from murphy.automation.vbox.screen import VirtualboxScreen
-from murphy.automation.vbox.factory import VirtualboxFactory
-
-
-__all__ = 'VirtualboxLoad', 'VirtualboxScreen', 'VirtualboxFactory'
